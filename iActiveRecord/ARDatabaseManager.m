e309f471fef408fffc5e1d86c101da1155b0071b
Marshall Weir <marshall.weir@gmail.com>
2013-02-26 14:22:39 -0500

    Add dispatch queue to synchronize database access

--- iActiveRecord/ARDatabaseManager.m
+++ iActiveRecord/ARDatabaseManager.m
@@ -30,12 +30,21 @@ static BOOL migrationsEnabled = YES;
 }
 
 + (id)sharedInstance {
-    @synchronized(self){
-        if(nil == instance){
-            instance = [[ARDatabaseManager alloc] init];
-        }
-        return instance;
-    }    
+    static ARDatabaseManager *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[ARDatabaseManager alloc] init];
+    });
+    return instance;
+}
+
++ (dispatch_queue_t)sqliteQueue {
+    static dispatch_queue_t queue;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        queue = dispatch_queue_create("com.iActiveRecord.sqlite", DISPATCH_QUEUE_SERIAL);
+    });
+    return queue;
 }
 
 - (id)init {
@@ -138,68 +147,74 @@ static BOOL migrationsEnabled = YES;
 
 - (NSArray *)columnsForTable:(NSString *)aTableName {
     NSString *sql = [NSString stringWithFormat:@"PRAGMA table_info(%@)", [aTableName quotedString]];
-    NSMutableArray *resultArray = nil;
-    char **results;
-    int nRows;
-    int nColumns;
-    const char *pszSql = [sql UTF8String];
-    if(SQLITE_OK == sqlite3_get_table(database,
-                                      pszSql,
-                                      &results,
-                                      &nRows,
-                                      &nColumns,
-                                      NULL))
-    {
-        resultArray = [NSMutableArray arrayWithCapacity:nRows++];
-        for(int i=0;i<nRows-1;i++){
-            int index = (i + 1)*nColumns + 1;
-            const char *pszValue = results[index];
-            if(pszValue){
-                [resultArray addObject:[NSString stringWithUTF8String:pszValue]];
+    __block NSMutableArray *resultArray = nil;
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        char **results;
+        int nRows;
+        int nColumns;
+        const char *pszSql = [sql UTF8String];
+        if(SQLITE_OK == sqlite3_get_table(database,
+                                          pszSql,
+                                          &results,
+                                          &nRows,
+                                          &nColumns,
+                                          NULL))
+        {
+            resultArray = [NSMutableArray arrayWithCapacity:nRows++];
+            for(int i=0;i<nRows-1;i++){
+                int index = (i + 1)*nColumns + 1;
+                const char *pszValue = results[index];
+                if(pszValue){
+                    [resultArray addObject:[NSString stringWithUTF8String:pszValue]];
+                }
             }
+            sqlite3_free_table(results);
+        }else
+        {
+            NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
         }
-        sqlite3_free_table(results);
-    }else
-    {
-        NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
-    }
+    });
     return resultArray;
 }
 
 //  select tbl_name from sqlite_master where type='table' and name not like 'sqlite_%'
 - (NSArray *)tables {
-    NSMutableArray *resultArray = nil;
-    char **results;
-    int nRows;
-    int nColumns;
-    const char *pszSql = [@"select tbl_name from sqlite_master where type='table' and name not like 'sqlite_%'" UTF8String];
-    if(SQLITE_OK == sqlite3_get_table(database,
-                                      pszSql,
-                                      &results,
-                                      &nRows,
-                                      &nColumns,
-                                      NULL))
-    {
-        resultArray = [NSMutableArray arrayWithCapacity:nRows++];
-        for(int i=0;i<nRows-1;i++){
-            for(int j=0;j<nColumns;j++){
-                int index = (i+1)*nColumns + j;
-                [resultArray addObject:[NSString stringWithUTF8String:results[index]]];
+    __block NSMutableArray *resultArray = nil;
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        char **results;
+        int nRows;
+        int nColumns;
+        const char *pszSql = [@"select tbl_name from sqlite_master where type='table' and name not like 'sqlite_%'" UTF8String];
+        if(SQLITE_OK == sqlite3_get_table(database,
+                                          pszSql,
+                                          &results,
+                                          &nRows,
+                                          &nColumns,
+                                          NULL))
+        {
+            resultArray = [NSMutableArray arrayWithCapacity:nRows++];
+            for(int i=0;i<nRows-1;i++){
+                for(int j=0;j<nColumns;j++){
+                    int index = (i+1)*nColumns + j;
+                    [resultArray addObject:[NSString stringWithUTF8String:results[index]]];
+                }
             }
+            sqlite3_free_table(results);
+        }else
+        {
+            NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
         }
-        sqlite3_free_table(results);
-    }else
-    {
-        NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
-    }
+    });
     return resultArray;
 }
 
 - (void)openConnection {
-    sqlite3_unicode_load();
-    if(SQLITE_OK != sqlite3_open([dbPath UTF8String], &database)){
-        NSLog(@"Couldn't open database connection: %s", sqlite3_errmsg(database));
-    }
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        sqlite3_unicode_load();
+        if(SQLITE_OK != sqlite3_open([dbPath UTF8String], &database)){
+            NSLog(@"Couldn't open database connection: %s", sqlite3_errmsg(database));
+        }
+    });
 }
 
 - (NSString *)tableName:(NSString *)modelName {
@@ -207,8 +222,10 @@ static BOOL migrationsEnabled = YES;
 }
 
 - (void)closeConnection {
-    sqlite3_close(database);
-    sqlite3_unicode_free();
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        sqlite3_close(database);
+        sqlite3_unicode_free();
+    });
 }
 
 - (NSNumber *)insertRecord:(NSString *)aRecordName withSqlQuery:(const char *)anSqlQuery {
@@ -217,9 +234,11 @@ static BOOL migrationsEnabled = YES;
 }
 
 - (void)executeSqlQuery:(const char *)anSqlQuery {
-    if(SQLITE_OK != sqlite3_exec(database, anSqlQuery, NULL, NULL, NULL)){
-        NSLog(@"Couldn't execute query %s : %s", anSqlQuery, sqlite3_errmsg(database));
-    }
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        if(SQLITE_OK != sqlite3_exec(database, anSqlQuery, NULL, NULL, NULL)){
+            NSLog(@"Couldn't execute query %s : %s", anSqlQuery, sqlite3_errmsg(database));
+        }
+    });
 }
  
 - (NSString *)documentsDirectory {
@@ -233,112 +252,116 @@ static BOOL migrationsEnabled = YES;
 }
 
 - (NSArray *)allRecordsWithName:(NSString *)aName withSql:(NSString *)aSqlRequest{
-    NSMutableArray *resultArray = nil;
-    NSString *propertyName;
-    id aValue;
-    Class Record;
-    char **results;
-    int nRows;
-    int nColumns;
-    const char *pszSql = [aSqlRequest UTF8String];
-    if(SQLITE_OK == sqlite3_get_table(database,
-                                      pszSql,
-                                      &results,
-                                      &nRows,
-                                      &nColumns,
-                                      NULL))
-    {
-        resultArray = [NSMutableArray arrayWithCapacity:nRows++];
-        Record = NSClassFromString(aName);
-        for(int i=0;i<nRows-1;i++){
-            id record = [Record new];
-            for(int j=0;j<nColumns;j++){
-                propertyName = [NSString stringWithUTF8String:results[j]];
-                int index = (i+1)*nColumns + j;
-                const char *pszValue = results[index];
-                
-                if(pszValue){
-                    ARColumn *column = [Record performSelector:@selector(columnNamed:) 
-                                                    withObject:propertyName];
-                    NSString *sqlData = [NSString stringWithUTF8String:pszValue];
-                    aValue = [column.columnClass performSelector:@selector(fromSql:) 
-                                                 withObject:sqlData];
-                }else{
-                    aValue = @"";
+    __block NSMutableArray *resultArray = nil;
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        NSString *propertyName;
+        id aValue;
+        Class Record;
+        char **results;
+        int nRows;
+        int nColumns;
+        const char *pszSql = [aSqlRequest UTF8String];
+        if(SQLITE_OK == sqlite3_get_table(database,
+                                          pszSql,
+                                          &results,
+                                          &nRows,
+                                          &nColumns,
+                                          NULL))
+        {
+            resultArray = [NSMutableArray arrayWithCapacity:nRows++];
+            Record = NSClassFromString(aName);
+            for(int i=0;i<nRows-1;i++){
+                id record = [Record new];
+                for(int j=0;j<nColumns;j++){
+                    propertyName = [NSString stringWithUTF8String:results[j]];
+                    int index = (i+1)*nColumns + j;
+                    const char *pszValue = results[index];
+                    
+                    if(pszValue){
+                        ARColumn *column = [Record performSelector:@selector(columnNamed:) 
+                                                        withObject:propertyName];
+                        NSString *sqlData = [NSString stringWithUTF8String:pszValue];
+                        aValue = [column.columnClass performSelector:@selector(fromSql:) 
+                                                     withObject:sqlData];
+                    }else{
+                        aValue = @"";
+                    }
+                    [record setValue:aValue forKey:propertyName];
                 }
-                [record setValue:aValue forKey:propertyName];
+                [resultArray addObject:record];
+                [record release];
             }
-            [resultArray addObject:record];
-            [record release];
+            sqlite3_free_table(results);
+        }else
+        {
+            NSLog(@"%@", aSqlRequest);
+            NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
         }
-        sqlite3_free_table(results);
-    }else
-    {
-        NSLog(@"%@", aSqlRequest);
-        NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
-    }
+    });
     return resultArray;
 }
 
 - (NSArray *)joinedRecordsWithSql:(NSString *)aSqlRequest {
-    NSMutableArray *resultArray = nil;
-    NSString *propertyName;
-    NSString *header;
-    id aValue;
-    char **results;
-    int nRows;
-    int nColumns;
-    const char *pszSql = [aSqlRequest UTF8String];
-    if(SQLITE_OK == sqlite3_get_table(database,
-                                      pszSql,
-                                      &results,
-                                      &nRows,
-                                      &nColumns,
-                                      NULL))
-    {
-        resultArray = [NSMutableArray arrayWithCapacity:nRows++];
-        for(int i=0;i<nRows-1;i++){
-            NSMutableDictionary *dictionary = [NSMutableDictionary new];
-            NSString *recordName = nil;
-            for(int j=0;j<nColumns;j++){
-                header = [NSString stringWithUTF8String:results[j]];
-                
-                recordName = [[header componentsSeparatedByString:@"#"] objectAtIndex:0];
-                propertyName = [[header componentsSeparatedByString:@"#"] objectAtIndex:1];
-                
-                Class Record = NSClassFromString(recordName);
-                
-                int index = (i+1)*nColumns + j;
-                const char *pszValue = results[index];
-                if(pszValue){
-                    ARColumn *column = [Record 
-                                                   performSelector:@selector(columnNamed:) 
-                                                        withObject:propertyName];
-                    NSString *sqlData = [NSString stringWithUTF8String:pszValue];
-                    aValue = [column.columnClass performSelector:@selector(fromSql:) 
-                                                 withObject:sqlData];
-                }else{
-                    aValue = @"";
+    __block NSMutableArray *resultArray = nil;
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        NSString *propertyName;
+        NSString *header;
+        id aValue;
+        char **results;
+        int nRows;
+        int nColumns;
+        const char *pszSql = [aSqlRequest UTF8String];
+        if(SQLITE_OK == sqlite3_get_table(database,
+                                          pszSql,
+                                          &results,
+                                          &nRows,
+                                          &nColumns,
+                                          NULL))
+        {
+            resultArray = [NSMutableArray arrayWithCapacity:nRows++];
+            for(int i=0;i<nRows-1;i++){
+                NSMutableDictionary *dictionary = [NSMutableDictionary new];
+                NSString *recordName = nil;
+                for(int j=0;j<nColumns;j++){
+                    header = [NSString stringWithUTF8String:results[j]];
+                    
+                    recordName = [[header componentsSeparatedByString:@"#"] objectAtIndex:0];
+                    propertyName = [[header componentsSeparatedByString:@"#"] objectAtIndex:1];
+                    
+                    Class Record = NSClassFromString(recordName);
+                    
+                    int index = (i+1)*nColumns + j;
+                    const char *pszValue = results[index];
+                    if(pszValue){
+                        ARColumn *column = [Record 
+                                                       performSelector:@selector(columnNamed:) 
+                                                            withObject:propertyName];
+                        NSString *sqlData = [NSString stringWithUTF8String:pszValue];
+                        aValue = [column.columnClass performSelector:@selector(fromSql:) 
+                                                     withObject:sqlData];
+                    }else{
+                        aValue = @"";
+                    }
+                    id currentRecord = [dictionary valueForKey:recordName];
+                    if(currentRecord == nil){
+                        currentRecord = [Record new];
+                        [dictionary setValue:currentRecord
+                                      forKey:recordName];
+                    }
+                    [currentRecord setValue:aValue
+                                     forKey:propertyName];
+    //                [dictionary setValue:aValue forKey:propertyName];
                 }
-                id currentRecord = [dictionary valueForKey:recordName];
-                if(currentRecord == nil){
-                    currentRecord = [Record new];
-                    [dictionary setValue:currentRecord
-                                  forKey:recordName];
-                }
-                [currentRecord setValue:aValue
-                                 forKey:propertyName];
-//                [dictionary setValue:aValue forKey:propertyName];
+                [resultArray addObject:dictionary];
+                [dictionary release];
             }
-            [resultArray addObject:dictionary];
-            [dictionary release];
+            sqlite3_free_table(results);
+        }else
+        {
+            NSLog(@"%@", aSqlRequest);
+            NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
         }
-        sqlite3_free_table(results);
-    }else
-    {
-        NSLog(@"%@", aSqlRequest);
-        NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
-    }
+    });
     return resultArray;
 }
 
@@ -357,30 +380,32 @@ static BOOL migrationsEnabled = YES;
 }
 
 - (NSInteger)functionResult:(NSString *)anSql {
-    char **results;
-    NSInteger resId = 0;
-    int nRows;
-    int nColumns;
-    const char *pszSql = [anSql UTF8String];
-    if(SQLITE_OK == sqlite3_get_table(database,
-                                      pszSql,
-                                      &results,
-                                      &nRows,
-                                      &nColumns,
-                                      NULL))
-    {
-        if(nRows == 0 || nColumns == 0){
-            resId = -1;
-        }else{
-            resId = [[NSString stringWithUTF8String:results[1]] integerValue];
-        }
+    __block NSInteger resId = 0;
+    dispatch_sync([ARDatabaseManager sqliteQueue], ^{
+        char **results;
+        int nRows;
+        int nColumns;
+        const char *pszSql = [anSql UTF8String];
+        if(SQLITE_OK == sqlite3_get_table(database,
+                                          pszSql,
+                                          &results,
+                                          &nRows,
+                                          &nColumns,
+                                          NULL))
+        {
+            if(nRows == 0 || nColumns == 0){
+                resId = -1;
+            }else{
+                resId = [[NSString stringWithUTF8String:results[1]] integerValue];
+            }
 
-        sqlite3_free_table(results);
-    }else
-    {
-        NSLog(@"%@", anSql);
-        NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
-    }
+            sqlite3_free_table(results);
+        }else
+        {
+            NSLog(@"%@", anSql);
+            NSLog(@"Couldn't retrieve data from database: %s", sqlite3_errmsg(database));
+        }
+    });
     return resId;
 }
 
