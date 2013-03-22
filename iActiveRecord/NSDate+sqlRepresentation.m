2a7f40411ffe40c763b19526f4ef80edfbfdc61a
Ryan Laughlin <rofreg@gmail.com>
2013-03-21 22:16:28 -0400

    NSDate SQL reads need to be doubles, not floats – otherwise, timestamps can be off by almost a minute

--- iActiveRecord/NSDate+sqlRepresentation.m
+++ iActiveRecord/NSDate+sqlRepresentation.m
@@ -16,7 +16,7 @@
 }
 
 + (id)fromSql:(NSString *)sqlData {
-    NSTimeInterval time = [sqlData floatValue];
+    NSTimeInterval time = [sqlData doubleValue];    // DON'T use floatValue – not precise enough
     return [NSDate dateWithTimeIntervalSince1970:time];
 }
 
