1d5d516c5ad0221c81c02a15d65e37657e5a10a1
Andrey Kan <list@livewings.com>
2016-09-27 01:18:09 -0400

    fix get class list

--- iActiveRecord/Classes/class_getSubclasses.h
+++ iActiveRecord/Classes/class_getSubclasses.h
@@ -10,6 +10,7 @@
 #import <objc/runtime.h>
 
 static NSArray *class_getSubclasses(Class parentClass) {
+    /*
     int numClasses = objc_getClassList(NULL, 0);
     Class classes[sizeof(Class) * numClasses];
     numClasses = objc_getClassList(classes, numClasses);
@@ -26,4 +27,35 @@ static NSArray *class_getSubclasses(Class parentClass) {
         [result addObject:classes[i]];
     }
     return result;
+     */
+     
+
+    
+    
+     int numClasses = objc_getClassList(NULL, 0);
+     Class *classes = NULL;
+    
+     classes = (__unsafe_unretained Class *)malloc(sizeof(Class) * numClasses);
+     numClasses = objc_getClassList(classes, numClasses);
+    
+     NSMutableArray *result = [NSMutableArray array];
+     for (NSInteger i = 0; i < numClasses; i++)
+     {
+     Class superClass = classes[i];
+     do
+     {
+     superClass = class_getSuperclass(superClass);
+     } while(superClass && superClass != parentClass);
+     
+     if (superClass == nil)
+     {
+     continue;
+     }
+     
+     [result addObject:classes[i]];
+     }
+     
+     free(classes);
+     
+     return result;
 }
